63119749e6c27dc00ef7d1e44660fa4ea81d9371
vishal <sagarvishal790@gmail.com>
2024-06-13 00:36:21 +0530

    removed the project section

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -8,19 +8,7 @@ const ProjectCard = () => {
   return (
     <div className='flex flex-col gap-4 w-[100%] h-[100vh] '>
 
-      <div className='flex justify-between lg:h-[300px] items-center lg:p-[20px] border border-purple-600 rounded-lg w-[100%] '>
-        <a href='https://namastereact-b909a.web.app/' target='_blank' className=''>
-          <img className='w-[800px] lg:w-[600px] transition-transform duration-300 transform hover:object-center' src={Swiggyimg} alt='sw'/>
-        </a>
-        <h1 className='lg:mx-[70px] text-white text-sm font-medium m-[8px] lg:text-md'>akjfjhashfjl ahfhkajfjkh  ahf ka fk a;fh ha khf ahfkhahfhuhausfa fahufhahfhuha fahuifa fiahifhkaf h</h1>
-      </div>
-
-      <div className='flex justify-between lg:h-[300px] items-center lg:p-[20px] border border-purple-600 rounded-lg w-[100%] '>
-        <h1 className='lg:mx-[70px] text-white text-sm font-medium m-[8px] lg:text-md'>akjfjhashfjl ahfhkajfjkh  ahf ka fk a;fh ha khf ahfkhahfhuhausfa fahufhahfhuha fahuifa fiahifhkaf h</h1>
-        <a href='https://cryptoapp-e5390.web.app/' target='_blank' className=''>
-          <img className='w-[800px] lg:w-[600px]' src={CoinGeeko} alt='sw'/>
-        </a>
-      </div>
+      <h1>this is project Card</h1>
 
     </div>
   )
